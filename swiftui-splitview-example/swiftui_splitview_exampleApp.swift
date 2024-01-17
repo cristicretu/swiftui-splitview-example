2cf604464caf5da104df17c0e78955e47675e42e
Cristian Cretu <crisemcr@gmail.com>
2024-01-17 21:54:47 +0200

    Initial Commit

---
+++ swiftui-splitview-example/swiftui_splitview_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_splitview_exampleApp.swift
+//  swiftui-splitview-example
+//
+//  Created by John Doe on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_splitview_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
